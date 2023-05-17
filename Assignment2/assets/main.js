2675ed1de5e0d4c80c8070f14c605a62dd0494c0
Ishaadhikary <isha.adhikary12354@gmail.com>
2023-05-17 08:47:20 +0545

    Adding Side Flow Contains

--- Assignment2/assets/main.js
+++ Assignment2/assets/main.js
@@ -1,3 +1,18 @@
+// // const slideImage = Array.from(imgCarousel.children);
+// // console.log(slideImage)
+// // // const slideWidth = slideImage[0].getBoundingClientRect().width
+// // const WIDTH = parseInt(`${500}px`)
+// // const DotNav = document.querySelector('.dot-nav')
+// // const dot = Array.from(DotNav.children);
+// // console.log(dot);
+// // const setSlidePosition = (imgCarousel, index)=>{
+// //     imgCarousel.style.transform = `translateX(${index * 100}%)`;
+// // }
+
+// // slideImage.forEach(setSlidePosition);
+// let count = 1;
+// console.log(imgCarousel);
+// const imgCarousel = document.querySelector('#carousel-image-wrapper');
 // const slideImage = Array.from(imgCarousel.children);
 // console.log(slideImage)
 // // const slideWidth = slideImage[0].getBoundingClientRect().width
@@ -5,84 +20,105 @@
 // const DotNav = document.querySelector('.dot-nav')
 // const dot = Array.from(DotNav.children);
 // console.log(dot);
-// const setSlidePosition = (imgCarousel, index)=>{
-//     imgCarousel.style.transform = `translateX(${index * 100}%)`;
+// const setSlidePosition = (slideImage, index)=>{
+//     slideImage.style.left = WIDTH * index + 'px';
 // }
 
 // slideImage.forEach(setSlidePosition);
-let count = 1;
-console.log(imgCarousel);
-const imgCarousel = document.querySelector('#carousel-image-wrapper');
-const slideImage = Array.from(imgCarousel.children);
-console.log(slideImage)
-// const slideWidth = slideImage[0].getBoundingClientRect().width
-const WIDTH = parseInt(`${500}px`)
-const DotNav = document.querySelector('.dot-nav')
-const dot = Array.from(DotNav.children);
-console.log(dot);
-const setSlidePosition = (slideImage, index)=>{
-    slideImage.style.left = WIDTH * index + 'px';
-}
+// // const initialLeftPositions = Array.from(imgCarousel).map((element) => element.style.left);
+// // imgCarousel.forEach((element, index) => {
+// //     element.style.left = initialLeftPositions[index];
+// //   });
+// function leftClick(){
+//     if (count >0 && count <=imgCarousel.length) { 
+//         imgCarousel[count].style.left = `${count * -500}px`;
+//         console.log(imgCarousel[count],count,'hehe');
+//         count--;  
+//       }   
+//     else if(count === 0)
+//     {
+//         count = imgCarousel.length-1;
+//         imgCarousel[count].style.left = `${count * +500}px`;
+//         console.log(imgCarousel[count],count,'nono');
+//         count--;
 
-slideImage.forEach(setSlidePosition);
-// const initialLeftPositions = Array.from(imgCarousel).map((element) => element.style.left);
-// imgCarousel.forEach((element, index) => {
-//     element.style.left = initialLeftPositions[index];
-//   });
-function leftClick(){
-    if (count >0 && count <=imgCarousel.length) { 
-        imgCarousel[count].style.left = `${count * -500}px`;
-        console.log(imgCarousel[count],count,'hehe');
-        count--;  
-      }   
-    else if(count === 0)
-    {
-        count = imgCarousel.length-1;
-        imgCarousel[count].style.left = `${count * +500}px`;
-        console.log(imgCarousel[count],count,'nono');
-        count--;
+//     }
+//     console.log(count,imgCarousel.length)
+//     let elementL = document.getElementById('testText'); 
+//         elementL.innerText = count;
+// }
 
-    }
-    console.log(count,imgCarousel.length)
-    let elementL = document.getElementById('testText'); 
-        elementL.innerText = count;
-}
+// function rightClick(){
+//     if(count<imgCarousel.length && count >=1){
+//     imgCarousel[count].style.left = `${count * +500}px`;
+//     console.log(imgCarousel[count],count);
+//     count ++
+//     }
+//     else{
+//         count = 0;
+//     }
+//     let elementR = document.getElementById('testText');
+//     elementR.innerText = count;
+//     console.log()
+// }
+
+// function leftClick(){
+//     if(curSlide ===0){
+//     imgCarousel[count].style.left = `${count * +500}px`;
+//     console.log(imgCarousel[count],count);
+//     count ++
+//     }
+//     else{
+//         count = 0;
+//     }
+//     let elementR = document.getElementById('testText');
+//     elementR.innerText = count;
+//     console.log()
+// }
+
+//  // let currentImg = imgCarousel.querySelector('img')
+//     // console.log(currentImg);
+//     // let nextImg = currentImg.nextElementSibling;
+//     // console.log(currentImg.nextElementSibling);
+//     // const amountToMove = -500;
+//     // nextImg.style.transform = `translateX(${amountToMove}px)`; 
+//     // currentImg.classList.add('slideImage[1]');
+//     // console.log(currentImg.classList.add('slideImage'))
+
+
+const imgCarousel = document.querySelectorAll('#carousel-image-wrapper img');
+
+let curSlide = 0
+let maxSlide = imgCarousel.length -1;
+
 
 function rightClick(){
-    if(count<imgCarousel.length && count >=1){
-    imgCarousel[count].style.left = `${count * +500}px`;
-    console.log(imgCarousel[count],count);
-    count ++
+    console.log('hello')
+    if(curSlide ===maxSlide){
+        curSlide = 0;
+        
+    console.log(curSlide,'Inside  if')
     }
+    
     else{
-        count = 0;
+        curSlide++;
+        console.log(curSlide,'Inside E')
     }
-    let elementR = document.getElementById('testText');
-    elementR.innerText = count;
-    console.log()
-}
+    imgCarousel.forEach((imgSlide,index)=>{
+        imgSlide.style.transform = `translateX(${500 * (index - curSlide)}px)`;
+    })}
+   
 
 function leftClick(){
     if(curSlide ===0){
-    imgCarousel[count].style.left = `${count * +500}px`;
-    console.log(imgCarousel[count],count);
-    count ++
-    }
-    else{
-        count = 0;
-    }
-    let elementR = document.getElementById('testText');
-    elementR.innerText = count;
-    console.log()
+        curSlide = maxSlide
+        }
+        else{
+            curSlide--;
+        }
+    imgCarousel.forEach((imgSlide,index)=>{
+            imgSlide.style.transform = `translateX(${500 * (index - curSlide)}px)`;
+        })
+    
+
 }
-
- // let currentImg = imgCarousel.querySelector('img')
-    // console.log(currentImg);
-    // let nextImg = currentImg.nextElementSibling;
-    // console.log(currentImg.nextElementSibling);
-    // const amountToMove = -500;
-    // nextImg.style.transform = `translateX(${amountToMove}px)`; 
-    // currentImg.classList.add('slideImage[1]');
-    // console.log(currentImg.classList.add('slideImage'))
-
-

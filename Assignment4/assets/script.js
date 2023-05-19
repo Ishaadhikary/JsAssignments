808fe8ba3c6672040000c6d12d80d7bcfed5a00e
Ishaadhikary <isha.adhikary12354@gmail.com>
2023-05-19 10:16:16 +0545

    Updating the format

--- Assignment4/assets/script.js
+++ Assignment4/assets/script.js
@@ -1,11 +1,11 @@
 const WIDTH = 22;
 const HEIGHT = 20;
-let score = 0
+let score = 0;
 
 class AntCrushingGame {
   constructor() {
     this.mainContainer = document.getElementById("main-container");
-    this.ants = [];//containg all the ants
+    this.ants = []; //containg all the ants
     this.mainContainer = document.getElementById("main-container");
     this.sideContainer = document.getElementById("side-container");
     this.intervalIds = []; // Store interval IDs
@@ -36,13 +36,13 @@ class AntCrushingGame {
       this.ants.push(ant);
     }
   }
-//Start the game
+  //Start the game
   startGame() {
     for (const ant of this.ants) {
       this.drawAnt(ant);
       const intervalId = setInterval(() => {
         this.moveAnt(ant);
-      }, 100);//From moving the ant
+      }, 100); //From moving the ant
       this.intervalIds.push(intervalId); // Store interval ID
     }
   }
@@ -55,24 +55,24 @@ class AntCrushingGame {
     ant_el.style.left = ant.x + "px";
     ant_el.style.bottom = ant.y + "px";
     this.mainContainer.appendChild(ant_el);
-//Removing the clicked ant
-    const removeAnt = () => { 
+    //Removing the clicked ant
+    const removeAnt = () => {
       console.log("Ant clicked");
-      console.log(ant_el)
+      console.log(ant_el);
       this.mainContainer.removeChild(ant_el);
       this.scoreboard();
     };
 
     ant_el.addEventListener("mousedown", removeAnt);
   }
-//Moving the ant
+  //Moving the ant
   moveAnt(ant) {
     const previousAnt = document.getElementById(ant.id);
     ant.x = ant.x + ant.dx;
     ant.y = ant.y + ant.dy;
     this.mainContainer.removeChild(previousAnt);
     this.drawAnt(ant);
-//Checking the collision with the Container
+    //Checking the collision with the Container
     if (
       ant.x + WIDTH > this.mainContainer.offsetWidth ||
       ant.x < 0 ||
@@ -82,10 +82,10 @@ class AntCrushingGame {
       ant.dx *= -1;
       ant.dy *= -1;
     }
-//Check the collision between ants
+    //Check the collision between ants
     this.collision();
   }
-//Generating the random values
+  //Generating the random values
   randomIntFromIntervalX() {
     let minX = 0;
     let maxX = this.mainContainer.offsetWidth - WIDTH;
@@ -98,7 +98,7 @@ class AntCrushingGame {
     return Math.floor(Math.random() * (maxY - minY + 1) + minY);
   }
 
-//Collison of Ant checking function
+  //Collison of Ant checking function
   collision() {
     for (let i = 0; i < this.ants.length; i++) {
       for (let j = i + 1; j < this.ants.length; j++) {
@@ -116,11 +116,11 @@ class AntCrushingGame {
           ant2.dx *= -1;
           ant2.dy *= -1;
           //Rotating the ant
-          if (ant1.dx< 0 || ant1.dy < 0){
+          if (ant1.dx < 0 || ant1.dy < 0) {
             const ant1Id = document.getElementById(ant1.id);
             ant1Id.style.transform = "rotate(180deg)";
           }
-          if (ant2.dx< 0 || ant2.dy < 0){
+          if (ant2.dx < 0 || ant2.dy < 0) {
             const ant2Id = document.getElementById(ant2.id);
             ant2Id.style.transform = "rotate(180deg)";
           }
@@ -129,34 +129,32 @@ class AntCrushingGame {
     }
     // console.log('Collision');
   }
-    scoreboard() {
-      // Increment the score
-      score++;
-    
-      this.sideContainer = document.getElementById("side-container");
-      let heading = this.sideContainer.querySelector("h1");
-    
-      if (!heading) {
-        // Create the <h1> element if it doesn't exist
-        heading = document.createElement("h1");
-        this.sideContainer.appendChild(heading);
-      }
-    
-      // Update score
-      heading.innerText = "Score: " + score;
-      
-      if(score === 20){
-        heading.innerText = "GAME OVER ";
-        alert("Congratulation you won!");
-        Object.assign(this.mainContainer.style, {
-          background: `url("bloodKill.svg") no-repeat center`,
-          backgroundSize: "300px",
-        });
-      }
-    }
-  
-  }
+  scoreboard() {
+    // Increment the score
+    score++;
 
+    this.sideContainer = document.getElementById("side-container");
+    let heading = this.sideContainer.querySelector("h1");
+
+    if (!heading) {
+      // Create the <h1> element if it doesn't exist
+      heading = document.createElement("h1");
+      this.sideContainer.appendChild(heading);
+    }
+
+    // Update score
+    heading.innerText = "Score: " + score;
+
+    if (score === 20) {
+      heading.innerText = "GAME OVER ";
+      alert("Congratulation you won!");
+      Object.assign(this.mainContainer.style, {
+        background: `url("bloodKill.svg") no-repeat center`,
+        backgroundSize: "300px",
+      });
+    }
+  }
+}
 
 // Playing Game:
 const game = new AntCrushingGame();
